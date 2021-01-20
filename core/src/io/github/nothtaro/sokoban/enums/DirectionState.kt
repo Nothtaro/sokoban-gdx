a5c44ce086748db08dba63a740e8cad007381a03
Nothtaro <nothnorth@outlook.com>
2021-01-21 00:45:24 +0900

    Add DirectionState Point parameter

--- core/src/io/github/nothtaro/sokoban/enums/DirectionState.kt
+++ core/src/io/github/nothtaro/sokoban/enums/DirectionState.kt
@@ -1,8 +1,10 @@
 package io.github.nothtaro.sokoban.enums
 
-enum class DirectionState {
-    NORTH,
-    EAST,
-    WEST,
-    SOUTH;
+import io.github.nothtaro.sokoban.util.Point
+
+enum class DirectionState(val point: Point) {
+    NORTH(Point(0,1)),
+    EAST(Point(1,0)),
+    WEST(Point(-1,-0)),
+    SOUTH(Point(0,-1));
 }
